6884d75b6554aeaf14e7b6f3c6c58184169f4d04
Pongsatorn <pongsatornpanchasoon@gmail.com>
2016-05-26 11:15:32 +0700

    ดีไซร์ หน้า ออเดอร์

--- app/src/main/java/pbru/panchasoon/pongsatorn/bookmeetingpbru/DetailActivity.java
+++ app/src/main/java/pbru/panchasoon/pongsatorn/bookmeetingpbru/DetailActivity.java
@@ -1,5 +1,6 @@
 package pbru.panchasoon.pongsatorn.bookmeetingpbru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -39,7 +40,12 @@ public class DetailActivity extends AppCompatActivity {
 
     public void clickOrder(View view) {
 
-    }
+        Intent intent = new Intent(DetailActivity.this, CalendaActivity.class);
+        intent.putExtra("User", getIntent().getStringArrayExtra("User"));
+        intent.putExtra("NameRoom", nameRoomString);
+        startActivity(intent);
+
+    } //clickOdder มันจะทำการเลือกห้องแล้วจะไปยัง ออเดอร์
 
     public void clickIncrease(View view) {
 
